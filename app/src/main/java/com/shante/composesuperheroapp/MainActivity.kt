f75d11396750a118da1b07946ab523b593d11757
Alex <alexsid.ShanTE@gmail.com>
2022-12-11 22:42:23 +0300

    added hero list and top app bar

--- app/src/main/java/com/shante/composesuperheroapp/MainActivity.kt
+++ app/src/main/java/com/shante/composesuperheroapp/MainActivity.kt
@@ -3,13 +3,36 @@ package com.shante.composesuperheroapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.shante.composesuperheroapp.data.HeroRepository
+import com.shante.composesuperheroapp.model.Hero
 import com.shante.composesuperheroapp.ui.theme.ComposeSuperHeroAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +40,106 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeSuperHeroAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                SuperHeroApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SuperHeroApp() {
+    Scaffold(
+        topBar = {
+            TopAppBar()
+        }
+    ) {
+        HeroList(listOfHeroes = HeroRepository.heroes)
+    }
+}
+
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = stringResource(id = R.string.super_heroes),
+            style = MaterialTheme.typography.h1
+        )
+    }
+}
+
+@Composable
+fun HeroList(listOfHeroes: List<Hero>, modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier
+            .background(MaterialTheme.colors.background)
+    ) {
+        items(listOfHeroes) {
+            HeroCard(hero = it)
+        }
+    }
+}
+
+@Composable
+fun HeroCard(
+    hero: Hero,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        elevation = 2.dp,
+        modifier = modifier.padding(8.dp),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .sizeIn(minHeight = 72.dp)
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = stringResource(hero.name),
+                    style = MaterialTheme.typography.h3,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = stringResource(hero.description),
+                    style = MaterialTheme.typography.body1,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+            Spacer(Modifier.width(16.dp))
+            Box(
+                modifier = Modifier
+                    .size(72.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            ) {
+                Image(
+                    painter = painterResource(hero.image),
+                    contentDescription = null,
+                    alignment = Alignment.TopCenter,
+                    contentScale = ContentScale.FillWidth
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeSuperHeroAppTheme {
-        Greeting("Android")
+fun ComposeSuperHeroLightTheme() {
+    ComposeSuperHeroAppTheme(darkTheme = false) {
+        HeroCard(hero = HeroRepository.heroes[3])
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeSuperHeroDarkTheme() {
+    ComposeSuperHeroAppTheme(darkTheme = true) {
+        SuperHeroApp()
     }
 }
